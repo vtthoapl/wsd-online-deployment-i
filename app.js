de6b50d2848475d813c4c17daee3a299f2917edd
vtthoapl <vtthoapl@gmail.com>
2023-09-07 13:45:24 +0300

    first

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
-  return new Response("Hello world!");
+  return new Response("Hello omniaaaaa!");
 };
 
 serve(handleRequest, { port: 7777 });
