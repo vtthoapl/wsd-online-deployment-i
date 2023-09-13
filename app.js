c8afb4aa90b44c9fecfd64de337014565e08977c
vtthoapl <vtthoapl@gmail.com>
2023-09-13 17:29:23 +0300

    changing  the content

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
-  return new Response("Hello omniaaaaa!");
+  return new Response(`
+  <h1> ONLINE DEPLOYMENT I </h1>
+  <p> I wish you all be successfull finishing the web software development course and finding the job </p>
+  <p> Kiitos </p>
+  `);
 };
 
 serve(handleRequest, { port: 7777 });
